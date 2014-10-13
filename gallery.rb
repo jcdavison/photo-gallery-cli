ad78caf66da4057af72568a59cd8f273d71596dc
John Davison <johncdavison@gmail.com>
2014-10-13 08:47:00 -0700

    Output html.

--- gallery.rb
+++ gallery.rb
@@ -1,9 +1,24 @@
-file = ARGV[0]
-
-path =  File.absolute_path(file)
-
 def img_tag(path_to_file)
   "<img src=\"#{path_to_file}\">"
 end
 
-puts img_tag(path)
+def html_page(content)
+  layout = <<-HTML
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>My Gallery</title>
+    </head>
+    <body>
+      <h1>My Gallery</h1>
+      #{content}
+    </body>
+    </html>
+  HTML
+  layout
+end
+
+file = ARGV[0]
+
+path =  File.absolute_path(file)
+puts html_page(img_tag(path))
