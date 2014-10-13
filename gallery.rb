c3fd95555274a459f67ba03525b5ce640b4d974e
John Davison <johncdavison@gmail.com>
2014-10-13 08:56:15 -0700

    Accept multiple image paths.

--- gallery.rb
+++ gallery.rb
@@ -1,7 +1,3 @@
-def img_tag(path_to_file)
-  "<img src=\"#{path_to_file}\">"
-end
-
 def html_page(content)
   layout = <<-HTML
     <!DOCTYPE html>
@@ -18,7 +14,31 @@ def html_page(content)
   layout
 end
 
-file = ARGV[0]
+def img_tag(path_to_file)
+  "<img src=\"#{path_to_file}\">"
+end
 
-path =  File.absolute_path(file)
-puts html_page(img_tag(path))
+def list_of_images(images)
+  list_of_image_tags = []
+  images.each do |image|
+    list_of_image_tags.push(img_tag(image))
+  end
+  list_of_image_tags
+end
+
+def list_of_file_paths(list_of_file_names)
+  absolute_file_paths = []
+  list_of_file_names.each do |file|
+    absolute_file_paths.push(File.absolute_path(file))
+  end
+  absolute_file_paths
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  images = ARGV
+  absolute_file_paths = list_of_file_paths(images)
+  images = list_of_images(absolute_file_paths)
+  content = images.join("\n")
+  puts html_page(content)
+end
